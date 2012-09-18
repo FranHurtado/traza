ae00c9e828ab1bc75e688ea91d62a32f01cb6f7e
Fran Hurtado <fhursan@gmail.com>
2012-09-18 09:59:01 +0200

    Modulo Trazabilidad Modulo APPC

---
+++ protected/modules/APPC/views/default/index.php
@@ -0,0 +1,23 @@
+<?php
+/* @var $this DefaultController */
+
+?>
+
+<div class="contentbox" id="mainmenu" style="width:700px;">
+	
+	<div class="title">
+		<h1>Men&uacute; de APPC</h1>
+	</div>
+	
+	<div id="menu">
+		<div class="buttonA"><p><a href="#">Operarios</a></p></div>
+		<div class="buttonA"><p><a href="#">Puntos Cr&iacute;ticos</a></p></div>
+		<div class="buttonA"><p><a href="#">Controles</a></p></div>
+		<div class="buttonA"><p><a href="#">Informes</a></p></div>
+		<div class="buttonA"><p><a href="#">Alertas</a></p></div>
+		<div class="clear"></div>
+	</div>
+
+</div>
+
+<a href="<?php echo Yii::app()->user->returnURL; ?>" class="backButton">Volver</a>
---
+++ protected/modules/Trazabilidad/views/default/index.php
@@ -0,0 +1,22 @@
+<?php
+/* @var $this DefaultController */
+?>
+
+<div class="contentbox" id="mainmenu" style="width:700px;">
+	
+	<div class="title">
+		<h1>Men&uacute; de Trazabilidad</h1>
+	</div>
+	
+	<div id="menu">
+		<div class="button"><p><a href="#">Proveedores</a></p></div>
+		<div class="button"><p><a href="#">Materias Primas</a></p></div>
+		<div class="button"><p><a href="#">Productos</a></p></div>
+		<div class="button"><p><a href="#">Recetas</a></p></div>
+		<div class="button"><p><a href="#">Operarios</a></p></div>
+		<div class="clear"></div>
+	</div>
+
+</div>
+
+<a href="<?php echo Yii::app()->user->returnURL; ?>" class="backButton">Volver</a>
